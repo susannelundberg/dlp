25d5c9aa26b4d6a03fe54e78b334d230e1469ba3
susannelundberg <lundbergsusanne@outlook.com>
2026-06-12 19:34:29 +0200

    Updated menu

--- src/utilities/menu.js
+++ src/utilities/menu.js
@@ -16,11 +16,11 @@ export const navbar = /*HTML*/ `
                     </label>
                 </li>
                 <li class="menu-item"><a href="/src/index.html">Start</a></li>
-                <li class="menu-item"><a href="/src/pages/program/program.html">Program</a></li>
-                <li class="menu-item"><a href="/src/pages/information/information.html">Information</a></li>
-                <li class="menu-item"><a href="/src/pages/weather/weather.html">Väder</a></li>
-                <li class="menu-item"><a href="/src/pages/maps/maps.html">Parkerna</a></li>
-                <li class="menu-item"><a href="/src/pages/my-page/my-page.html">Min sida</a></li>
+                <li class="menu-item"><a href="/pages/program/program.html">Program</a></li>
+                <li class="menu-item"><a href="/pages/information/information.html">Information</a></li>
+                <li class="menu-item"><a href="/pages/weather/weather.html">Väder</a></li>
+                <li class="menu-item"><a href="/pages/maps/maps.html">Parkerna</a></li>
+                <li class="menu-item"><a href="/pages/my-page/my-page.html">Min sida</a></li>
             </ul>
          </nav>
          <!-- Navbar stop -->
